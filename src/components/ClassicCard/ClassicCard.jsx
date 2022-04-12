4c0629f422ae76ca6023d466118e891d6dfe6c8e
ChrisMak <cmakride@gmail.com>
2022-04-12 11:55:35 -0400

    Classic Card

--- src/components/ClassicCard/ClassicCard.jsx
+++ src/components/ClassicCard/ClassicCard.jsx
@@ -1,10 +1,20 @@
-const ClassicCard = () => {
+import { Link } from 'react-router-dom'
+
+const ClassicCard = ({classic}) => {
   return ( 
   
-  <>
-  <h2>CLASSIC INSERT HERE</h2>
   
-  </> );
+  <Link to={`/classics/${classic.id}`} className="card">
+    <div className='card-content'>
+      <div className='card-img-container'>
+        <img src={classic.image} alt={`${classic.name}`} />
+      </div>
+      <h2 className='card-title'>{classic.name}</h2>
+      <p>{classic.category}</p>
+      <p><small>{classic.description}</small></p>
+    </div>
+  </Link>
+  )
 }
  
 export default ClassicCard;
